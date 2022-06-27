a708e051227b635c01c3208184956ac4cc74621b
Marina Galo <51497313+batistagalo@users.noreply.github.com>
2022-06-27 12:53:46 -0300

    Update Carro.java

--- src/main/java/br/edu/ufabc/poo/carro/Carro.java
+++ src/main/java/br/edu/ufabc/poo/carro/Carro.java
@@ -1,24 +1,28 @@
 package br.edu.ufabc.poo.carro;
-
 public class Carro {
-
-	/*
-	 * Crie uma classe chamada "Carro" que terá métodos para acelerar, frear e
-	 * informar a velocidade do carro. Ao ser criada uma instância dessa classe, a
-	 * potência do motor e a velocidade máxima devem ser informadas (e seus valores
-	 * não podem ser alterados depois da criação/instanciação do objeto). Crie um
-	 * algoritmo para acelerar o carro de acordo com sua potência do motor (cada vez
-	 * que acelerar, soma-se o valor da potência do motor), não deixando o mesmo
-	 * ultrapassar a velocidade máxima (ou seja, se a velocidade passar da
-	 * velocidade máxima, ela terá o valor da velocidade máxima). A aula sobre
-	 * Classes vai ajudar a criar a classe Carro.
-	 * 
-	 * 
-	 * Os testes automatizados estão disponíveis, assim, espera-se que ao
-	 * implementar toda a classe Carro, todos os testes passarão.
-	 * 
-	 * 
-	 * Crie também uma classe chamada "Principal" e simule dois carros acelerando e
-	 * freando algumas vezes, imprimindo no final os valores de suas velocidades.
-	 */
+	private int potencia;
+	private int vMaxima;
+	private int velocidade;
+	
+	// Construtor
+	public Carro(int potencia, int vMaxima) {
+		this.potencia = potencia;
+		this.vMaxima = vMaxima;
+	}
+	
+	// Classes
+	public int getVelocidade() {
+		return velocidade;
+	}
+	
+	public void frear() {
+		velocidade = velocidade/2;
+	}
+	
+	public void acelerar() {
+		velocidade += potencia;
+		if (velocidade > vMaxima) {
+			velocidade = vMaxima;
+		}
+	}
 }
